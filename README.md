1973e092de05ccc7a351b9861b2e6d8b1f96c6c5
Joe Fitzgerald <jfitzgerald@pivotal.io>
2015-11-16 08:36:28 -0700

    MVP Provider

---
+++ README.md
@@ -0,0 +1,4 @@
+# autocomplete-go
+
+An [autocomplete-plus](https://github.com/atom/autocomplete-plus) provider for
+the [go](https://golang.org) language that uses [gocode](https://github.com/nsf/gocode) to provide suggestions.
---
+++ spec/main-spec.js
@@ -0,0 +1,27 @@
+'use babel'
+
+describe('autocomplete-go', () => {
+  let mainModule = null
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('go-config').then(() => {
+        return atom.packages.activatePackage('autocomplete-go')
+      }).then((pack) => {
+        mainModule = pack.mainModule
+      })
+    })
+  })
+
+  describe('when the autocomplete-go package is activated', () => {
+    it('activates successfully', () => {
+      expect(mainModule).toBeDefined()
+      expect(mainModule).toBeTruthy()
+      expect(mainModule.getProvider).toBeDefined()
+      expect(mainModule.getGoconfig).toBeDefined()
+      expect(mainModule.consumeGoconfig).toBeDefined()
+      expect(mainModule.getGoconfig()).toBeTruthy()
+      expect(mainModule.getProvider()).toBeTruthy()
+    })
+  })
+})
